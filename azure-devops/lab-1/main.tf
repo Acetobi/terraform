5412f260ef5dcb5bcdbf3f1e9fafcbced518ec71
Vladyslav Lebediev <Vladyslav_Lebediev@epam.com>
2021-11-23 19:58:36 +0200

    remove terraform module

--- azure-devops/lab-1/main.tf
+++ azure-devops/lab-1/main.tf
@@ -1,10 +1,10 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
- terraform {
-  required_providers {
-    azurerm = "2.52.0"
-  }
-}
+#  terraform {
+#   required_providers {
+#     azurerm = "2.52.0"
+#   }
+# }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
