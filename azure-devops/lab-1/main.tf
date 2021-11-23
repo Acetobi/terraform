c871605b0b2cd567a892cb8714309de78e8844cf
Vladyslav Lebediev <Vladyslav_Lebediev@epam.com>
2021-11-23 19:43:19 +0200

    change terraform conf

--- azure-devops/lab-1/main.tf
+++ azure-devops/lab-1/main.tf
@@ -1,11 +1,8 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
-terraform {
+ terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
+    azurerm = "2.52.0"
   }
 }
 
